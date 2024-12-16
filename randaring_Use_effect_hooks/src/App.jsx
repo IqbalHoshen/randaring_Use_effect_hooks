3117f4e8cd65d037b738befae7873b6fff573ab1
Iqbal Hoshen Saif <iqbalhoshensaif@gmail.com>
2024-12-16 13:09:18 +0600

    first commit

---
+++ randaring_Use_effect_hooks/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>REACT CORE CONCEPTS RECAP</h1>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>props</li>
+        <li>Event handler</li>
+        <li>State</li>
+        <li>load data</li>
+      </ol>
+      <hr />
+    </>
+  )
+}
+
+export default App
